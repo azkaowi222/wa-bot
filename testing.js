793afc5b38e0a7d66ca078760ba94538f9cb6571
azkaowi222 <azkaowi222@gmail.com>
2024-10-25 20:34:19 +0700

    UPDATE ENV

--- testing.js
+++ testing.js
@@ -1,12 +1,12 @@
 // Make sure to include these imports:
 // import { GoogleGenerativeAI } from "@google/generative-ai";
+const dotenv = require("dotenv");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
+dotenv.config();
 const answer = async (message) => {
   try {
-    const genAI = new GoogleGenerativeAI(
-      "AIzaSyA8bYVcFBDUAsXi6YsppZjDuV7UXAcnWIo"
-    );
+    const genAI = new GoogleGenerativeAI(process.env.API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const result = await model.generateContent(message);
     const answer = result.response.text();
